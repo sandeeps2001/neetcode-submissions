e9d60f276c6cde60547849b75ba27c5bef52d557
sandeeps2001 <126564997+sandeeps2001@users.noreply.github.com>
2026-07-09 16:47:03 -0400

    Add: binary-search - submission-12

---
+++ Data Structures & Algorithms/binary-search/submission-12.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let left = 0
+        let right  = nums.length-1
+        while (right >= left){
+        let half = Math.floor(left + right -left/2)
+        if(nums[half] === target){
+            return half
+        }
+       else if(nums[half] > target){
+            right  = half-1
+        }
+        else{
+            left = half+1
+        }
+    }
+
+    return -1
+    }
+}
