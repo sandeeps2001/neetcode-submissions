47deb1d918b77d28085989a6fe48ac064a91c50f
sandeeps2001 <126564997+sandeeps2001@users.noreply.github.com>
2026-07-26 18:54:25 -0400

    Add: two-integer-sum - submission-27

---
+++ Data Structures & Algorithms/two-integer-sum/submission-27.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+     let hash ={}
+     for(let i = 0 ; i<nums.length ; i++){
+        hash[nums[i]] = i
+     }
+     for(let i = 0 ; i < nums.length ; i++ ){
+     if(hash[target - nums[i]] && i!= hash[target - nums[i]])return [i , hash[target- nums[i]]]
+     }
+
+
+}
+}
