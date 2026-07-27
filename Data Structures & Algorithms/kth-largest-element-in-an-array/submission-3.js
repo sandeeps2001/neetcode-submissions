dfdcff5b2f38037c2e315e98a93873dd67d88efd
sandeeps2001 <126564997+sandeeps2001@users.noreply.github.com>
2026-07-27 19:43:48 -0400

    Add: kth-largest-element-in-an-array - submission-3

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-3.js
@@ -0,0 +1,50 @@
+class Solution {
+    add(val){
+        if(this.heap.length < this.k){
+            this.heap.push(val)
+            this.up(this.heap.length-1)
+        }
+        else if(this.heap[0] < val){
+              this.heap[0] = val
+              this.down(0)
+        }
+
+    }
+
+    up(idx){
+        while (idx > 0){
+            let parent = Math.floor((idx-1)/2)
+            if(this.heap[idx] > this.heap[parent])break
+            [this.heap[idx],this.heap[parent]] = [this.heap[parent], this.heap[idx]]
+            idx = parent
+        }
+
+    }
+
+    down(idx){
+
+    while(idx<this.k){  
+        let smallest = idx
+        let left = 2* idx +1
+        let right = 2 * idx +2
+        if(left < this.k && this.heap[left] < this.heap[smallest]) smallest = left
+        if(right < this.k && this.heap[right] < this.heap[smallest]) smallest = right 
+        if(smallest == idx)break
+        [this.heap[idx],this.heap[smallest]] = [this.heap[smallest] ,this.heap[idx]]
+        idx = smallest
+     }
+    }
+    /**
+     * 
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    findKthLargest(nums, k) {
+     this.heap = []
+     this.k = k
+     for(let n of nums)this.add(n)
+     return this.heap[0]
+
+    }
+}
