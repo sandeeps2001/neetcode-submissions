67431cba79e4dc087e55284250ae60216b2819db
sandeeps2001 <126564997+sandeeps2001@users.noreply.github.com>
2026-08-02 16:03:21 -0400

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    dfs(n ,curr ,open ,close){
+          if(curr.length === 2*n){
+            this.res.push(curr.join(''))
+            return 
+          }
+          if(open < n){
+            curr.push('(')
+            this.dfs(n,curr,open+1,close)
+            curr.pop()
+          }
+          if(close < open){
+            curr.push(')')
+            this.dfs(n,curr,open,close+1)
+            curr.pop()
+          }
+    }
+    /**
+     * @param {number} n
+     * @return {string[]}
+     */
+    generateParenthesis(n) {
+        this.res = []
+        this.dfs(n,[],0, 0)
+        return this.res
+    }
+}
